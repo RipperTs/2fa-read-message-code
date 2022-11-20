fbac126adcb02f9bba45cc154d0a68b61cba232b
ripper <wangyifani@foxmail.com>
2022-11-20 18:58:24 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,22 @@
+> 读取最近短信验证码消息到剪切板, 用于后续的网页内容填写
+
+## 注意事项
+本插件需要在iPhone手机上开启短信转发,并且在Mac的安全隐私设置中给uTools完全磁盘访问权限
+    - 短信转发设置: 设置->信息->短信转发  (`允许通过其他已登录iMessage信息账户接收iPhone短信`)
+
+## 使用
+1. Type `2fa` to trigger uTools
+2. Type`⌘ C` or `⏎` to copy captcha
+
+## 效果
+
+![img.png](img.png)
+![img_1.png](img_1.png)
+
+## 原理
+Mac上短信数据库位置是放在 `Library/Messages/chat.db` 通过插件读取到并解析出来最近的短信验证码, 然后通过剪切板复制到剪切板
+
+
+## 鸣谢
+- 灵感来源: [squatto](https://github.com/squatto/alfred-imessage-2fa)
+- uTools官方
---
+++ src/preload.js
@@ -0,0 +1,295 @@
+var path = require('path');
+var sqlite3 = require('sqlite3').verbose();
+var Q = require('q');
+const {utils, dateUtils} = require('@stacker/alfred-utils');
+const lookBackMinutes = 10;
+
+(async function () {
+    const messages = await readLatestMessage();
+    iMdisconnect();
+    let items = [];
+    if (messages.length) {
+        items.push(...messages.reduce((res, messageObj) => {
+            const msg = preProcessMessage(messageObj.text);
+            if (!msg.trim()) {
+                return res;
+            }
+            const captcha = readCaptchaFromMessage(msg);
+            if (captcha) {
+                const subject = readSubjectFromMessage(msg);
+                res.push(utils.buildItem({
+                    title: `${captcha}`,
+                    subtitle: `${subject ? `Sender：${subject} ` : ''}${dateUtils.formatToCalendar(messageObj.message_date)}，⏎ to Copy`,
+                    arg: captcha,
+                    text: {
+                        largetype: messageObj.text, copy: captcha
+                    }
+                }));
+            }
+            return res;
+        }, []))
+    }
+    if (items.length) {
+        return utils.printScriptFilter({
+            items
+        });
+    } else {
+        utils.printScriptFilter({
+            items: [utils.buildItem({
+                title: 'There is no authentication code', subtitle: '⏎ to view Messages App', arg: 'view_message'
+            })]
+        });
+    }
+})();
+
+
+function preProcessMessage(msg) {
+    return msg.replace(/((https?|ftp|file):\/\/|www\.)[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/i, '');
+}
+
+/**
+ * 读取短信验证码
+ * @param msg
+ * @returns {null|*}
+ */
+function readCaptchaFromMessage(msg) {
+    try {
+        return msg.match(/\d{4,6}/)[0];
+    } catch (e) {
+        return null;
+    }
+}
+
+/**
+ * 读取短信主题
+ * @param msg
+ * @returns {null|*}
+ */
+function readSubjectFromMessage(msg) {
+    try {
+        return msg.match(/【.+】/)[0];
+    } catch {
+        return null;
+    }
+}
+
+function readLatestMessage() {
+    return new Promise((resolve) => {
+        iMgetDb(function (err, db) {
+            db.all(`
+            select
+            message.rowid,
+            ifnull(handle.uncanonicalized_id, chat.chat_identifier) AS sender,
+            message.service,
+            datetime(message.date / 1000000000 + 978307200, 'unixepoch', 'localtime') AS message_date,
+            message.text
+        from
+            message
+                left join chat_message_join
+                        on chat_message_join.message_id = message.ROWID
+                left join chat
+                        on chat.ROWID = chat_message_join.chat_id
+                left join handle
+                        on message.handle_id = handle.ROWID
+        where
+            message.is_from_me = 0
+            and message.text is not null
+            and length(message.text) > 0
+            and (
+                message.text glob '*[0-9][0-9][0-9][0-9]*'
+                or message.text glob '*[0-9][0-9][0-9][0-9][0-9]*'
+                or message.text glob '*[0-9][0-9][0-9][0-9][0-9][0-9]*'
+                or message.text glob '*[0-9][0-9][0-9]-[0-9][0-9][0-9]*'
+                or message.text glob '*[0-9][0-9][0-9][0-9][0-9][0-9][0-9]*'
+                or message.text glob '*[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]*'
+            )
+            
+            and datetime(message.date / 1000000000 + strftime('%s', '2001-01-01'), 'unixepoch', 'localtime')
+                    >= datetime('now', '-${lookBackMinutes} minutes', 'localtime')
+        order by
+            message.date desc
+        limit 50`, function (err, res) {
+                resolve(res);
+            });
+        });
+    });
+}
+
+window.exports = {
+    'smsVerificationCode': {
+        mode: 'list',
+        args: {
+            enter: (action, callbackSetList) => {
+                readLatestMessage().then((message) => {
+                    if (message && message.length > 0) {
+                        let messageList = [];
+                        for (let i = 0; i < message.length; i++) {
+                            var code = readCaptchaFromMessage(message[i].text);
+                            messageList.push({
+                                title: readSubjectFromMessage(message[i].text),
+                                description: message[i].text,
+                                code: code,
+                            })
+                        }
+                        callbackSetList(messageList)
+                    } else {
+                        callbackSetList([
+                            {
+                                title: "未读取到验证码",
+                                description: '可能还没有收到验证码',
+                                code: ''
+                            }
+                        ])
+                    }
+                })
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                let code = itemData.code
+                if (code !== '') {
+                    window.utools.copyText(code)
+                }
+                window.utools.outPlugin()
+            }
+        }
+    },
+}
+
+
+function iMconnect() {
+    var deferred = Q.defer();
+    var db = new sqlite3.Database(
+        path.join(process.env["HOME"], '/Library/Messages/chat.db'),
+        sqlite3.OPEN_READONLY,
+        function (err, res) {
+            if (err) {
+                console.log("数据库连接失败", err)
+                return deferred.reject(err)
+            }
+
+            return deferred.resolve(db);
+        });
+
+    return deferred.promise;
+}
+
+function iMgetDb(cb) {
+    var args = arguments;
+
+    // nodeify?
+    iMconnect().then(function (db) {
+        cb(null, db);
+    }, function (err) {
+        cb(err);
+    });
+}
+
+window.iMgetRecipients = function (string, cb) {
+    if (typeof string == 'function') {
+        cb = string;
+        string = false;
+    }
+    iMconnect().done(function (db) {
+        var where = "";
+        // Maybe dangerous, check SQLlite doc
+        if (string && string != "") where = " WHERE id LIKE '%" + string + "%'";
+        db.all("SELECT * FROM `handle`" + where, cb);
+    });
+};
+
+window.iMgetRecipientById = function (id, details, cb) {
+    if (typeof details == 'function') {
+        cb = details;
+        details = false;
+    }
+    iMconnect().done(function (db) {
+        db.get("SELECT * FROM `handle` WHERE ROWID = $id", {$id: id}, function (err, recipient) {
+            if (!details) return cb(err, recipient);
+            if (err) return cb(err);
+            db.all("SELECT * FROM `message` WHERE handle_id = $id", {$id: id}, function (err, messages) {
+                if (err) return cb(err);
+                recipient.messages = messages;
+                cb(err, recipient);
+            });
+        });
+    });
+};
+
+window.iMgetMessages = function (string, details, cb) {
+    if (typeof string == 'function') {
+        cb = string;
+        string = false;
+    }
+    if (typeof details == 'function') {
+        cb = details;
+        details = false;
+    }
+
+    iMconnect().done(function (db) {
+        var where = "";
+        var join = "";
+        // Maybe dangerous, check SQLlite doc
+        if (string && string != "") where = " WHERE `message`.text LIKE '%" + string + "%'";
+        if (details) join = " JOIN `handle` ON `handle`.ROWID = `message`.handle_id";
+        db.all("SELECT * FROM `message`" + join + where, cb);
+    });
+};
+
+window.iMgetMessagesFromId = function (id, string, cb) {
+    if (typeof string == 'function') {
+        cb = string;
+        string = false;
+    }
+
+    iMconnect().done(function (db) {
+        var where = "";
+        // Maybe dangerous, check SQLlite doc
+        if (string && string != "") where = " AND text LIKE '%" + string + "%'";
+        db.all("SELECT * FROM `message` WHERE handle_id = $id" + where, {$id: id}, function (err, messages) {
+            cb(err, messages);
+        });
+    });
+};
+
+window.iMgetAttachmentsFromId = function (id, cb) {
+    iMconnect().done(function (db) {
+        db.all("SELECT * FROM `message` \
+      INNER JOIN `message_attachment_join` \
+      ON `message`.ROWID = `message_attachment_join`.message_id \
+      INNER JOIN `attachment` \
+      ON `attachment`.ROWID = `message_attachment_join`.attachment_id \
+      WHERE `message`.handle_id = $id", {$id: id}, function (err, messages) {
+            cb(err, messages);
+        });
+    });
+};
+
+window.iMgetAttachmentById = function (id, cb) {
+    iMconnect().done(function (db) {
+        db.get("SELECT * FROM `message` \
+      INNER JOIN `message_attachment_join` \
+      ON `message`.ROWID = `message_attachment_join`.message_id \
+      INNER JOIN `attachment` \
+      ON `attachment`.ROWID = `message_attachment_join`.attachment_id \
+      WHERE `message_attachment_join`.attachment_id = $id", {$id: id}, function (err, messages) {
+            cb(err, messages);
+        });
+    });
+};
+
+window.iMgetAttachments = function (cb) {
+    iMconnect().done(function (db) {
+        db.all("SELECT * FROM `message_attachment_join` \
+      INNER JOIN `message` \
+      ON `message`.ROWID = `message_attachment_join`.message_id \
+      INNER JOIN `attachment` \
+      ON `attachment`.ROWID = `message_attachment_join`.attachment_id", cb);
+    });
+};
+
+window.iMdisconnect = function () {
+    iMconnect().done(function (db) {
+        db.close();
+    });
+};
